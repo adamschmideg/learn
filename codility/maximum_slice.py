0082956a2ef2435df7516f1ce04faec6172cfeaa
Adam Schmideg <adam@schmideg.net>
2020-12-14 15:38:03 +0100

    Max slice not perfect

---
+++ codility/maximum_slice.py
@@ -0,0 +1,34 @@
+def solution(A):
+    # write your code in Python 3.6
+
+    best_index = 0
+    best = A[0]
+    total = best
+    for i in range(1, len(A)):
+        elem = A[i]
+        total += elem
+        if total > best:
+            best_index = i
+            best = total
+
+    #print(f"{best_index}=>{best}")
+    decrease = 0
+    least = 0
+    for i in range(0, best_index):
+        decrease += A[i]
+        if decrease < least:
+            least = decrease
+    #print(f"least: {least}")
+    return best - least
+
+tests = [
+    [[3,2,-6,4,0], 5],
+    [[-1, 2, 3, -2, 1], 5],
+    [[-5], -5],
+    [[-2, 1, 1], 2]
+]
+if __name__ == '__main__':
+    for input, want in tests:
+        got = solution(input)
+        assert got == want, f"{input} => {got}, not {want}"
+
