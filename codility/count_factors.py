785f941d6bdbe14903f1c4184e96f6fcc229e5c2
Adam Schmideg <adam@schmideg.net>
2020-12-17 12:36:12 +0100

    prime factorization

--- codility/count_factors.py
+++ codility/count_factors.py
@@ -5,14 +5,19 @@ def factors(N):
     factors = {}
     cur_factor = 2
     while num > 1 and cur_factor <= math.sqrt(num) + 2:
+        print(num, cur_factor)
         if num % cur_factor == 0:
             factors[cur_factor] = factors.get(cur_factor, 0) + 1
             num = int(num / cur_factor)
         else:
             cur_factor += 1
     if num != 1:
-        factors[num] = 1
-    print(f"{num} {N} => {factors}")
+        factors[num] = factors.get(num, 0) + 1
+    print(f"{N} => {factors}")
+    check = 1
+    for k, v in factors.items():
+        check *= k**v
+    assert check == N, f"{check} != {N} ({N / check}"
     return factors
 
 
@@ -25,14 +30,16 @@ def solution(N):
     return result
 
 tests = [
-    [2, 2],
-    [3, 2],
-    [5, 2],
-    [6, 4],
-    [8, 4],
-    [9, 3],
-    [24, 8],
-    [42, 8],
+#    [2, 2],
+#    [3, 2],
+#    [5, 2],
+#    [6, 4],
+#    [8, 4],
+#    [9, 3],
+#    [24, 8],
+#    [42, 8],
+#    [4999696, 45],
+    [49, 3],
 ]
 
 for t in tests:
