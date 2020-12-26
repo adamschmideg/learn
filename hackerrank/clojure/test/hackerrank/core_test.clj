191ea11d0408a05f97bbdaafcbbd474f21f88b2d
Adam Schmideg <adam@schmideg.net>
2020-12-26 15:05:47 +0100

    starting to work on clojure file-based test

--- hackerrank/clojure/test/hackerrank/core_test.clj
+++ hackerrank/clojure/test/hackerrank/core_test.clj
@@ -1,6 +1,7 @@
 (ns hackerrank.core-test
   (:require [clojure.test :refer :all]
-            [hackerrank.core :refer :all]))
+            [hackerrank.core :refer :all]
+            [clojure.java.io :as io]))
 
 (deftest maximum-palindromes-test
   (testing "length"
@@ -17,4 +18,17 @@
       "amim" 2))
   (testing "short"
     (are [s start end n] (= n (maximum-palindromes s start end))
-      "madamimadam" 4 7 2)))
+      "madamimadam" 4 7 2))
+  (testing "test case 12"
+    (let [s "wldsfubcsxrryqpqyqqxrlffumtuwymbybnpemdiwyqz"]
+      (are [start end n] (= n (maximum-palindromes s start end))
+        31 38 8
+        29 33 3
+        13 34 60480
+        4 17 144
+        15 26 16
+        2 10 7
+        10 33 544320)))
+  (testing "from resource"
+    (let [input (-> "resources/input.txt" io/resource slurp)])))
+
